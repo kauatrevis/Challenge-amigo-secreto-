3b1cca69a2c7e20ddd5d8e68c025ab3edc59c4cc
kauatrevis <kauatrevisan10@gmail.com>
2025-09-05 14:12:06 -0300

    revisão projeto

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
 function adicionarAmigo() {
     const input = document.getElementById('amigo');
     const nomeAmigo = input.value.trim();
+
     if (nomeAmigo) {
         const lista = document.getElementById('listaAmigos');
+        const amigos = lista.getElementsByTagName('li');
+
+        // transforma em array e pega só os textos
+        const nomes = Array.from(amigos).map(item => item.textContent);
+
+        // verifica se já existe
+        if (nomes.includes(nomeAmigo)) {
+            alert('Esse nome já foi adicionado.');
+            return;
+        }
+
+        // cria o <li> e adiciona na lista
         const item = document.createElement('li');
         item.textContent = nomeAmigo;
-        lista.appendChild(item);
+        lista.appendChild(item);  
         input.value = '';
     } else {
-        alert9('Por favor, insira um nome válido.');
-    }
+        alert('Por favor, insira um nome válido.');
+    }   
 }
+
 function sortearAmigo() {
     const lista = document.getElementById('listaAmigos');
     const amigos = lista.getElementsByTagName('li');
